6318aaa55b964c2c7d0d7d432f329f5c247dde2f
Ida Jankowska <ida.jankowska@onet.pl>
2026-04-22 21:21:03 +0200

    Go back to JS

--- cypress.config.js
+++ cypress.config.js
@@ -4,7 +4,7 @@ module.exports = defineConfig({
   allowCypressEnv: false,
 
   e2e: {
-    specPattern: "cypress/tests/**/*.cy.ts",
+    specPattern: "cypress/tests/**/*.cy.js",
 
     setupNodeEvents(on, config) {
       return config;
